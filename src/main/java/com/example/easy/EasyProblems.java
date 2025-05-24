f844688552a1455666dcbd7ada43f23d7c81943b
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-05-24 11:21:58 +0300

    add solution for Kids With the Greatest Number of Candies

--- src/main/java/com/example/easy/EasyProblems.java
+++ src/main/java/com/example/easy/EasyProblems.java
@@ -1,6 +1,8 @@
 package com.example.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class EasyProblems {
@@ -63,5 +65,18 @@ public class EasyProblems {
     }
 
     return "";
+  }
+
+  // 1431. Kids With the Greatest Number of Candies
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    List<Boolean> result = new ArrayList<>(Collections.nCopies(candies.length, false));
+    int maxCandies = Arrays.stream(candies).max().getAsInt();
+
+    for (int i = 0; i < candies.length; i++) {
+      if (candies[i] + extraCandies >= maxCandies)
+        result.set(i, true);
+    }
+
+    return result;
   }
 }
